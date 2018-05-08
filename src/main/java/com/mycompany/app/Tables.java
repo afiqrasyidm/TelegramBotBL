09689b0cafa699a3967f711983704eabb936cb72
Galuh Kautsar <gabutrasar@gmail.com>
2018-05-08 09:51:24 +0700

    Fix coding style

--- src/main/java/com/mycompany/app/Tables.java
+++ src/main/java/com/mycompany/app/Tables.java
@@ -1,17 +1,13 @@
 package com.mycompany.app;
 
-
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
 /**
  * @author Igor Polevoy on 11/16/15.
  */
-public class Tables{
-
-
- @Table("hackaton.User")
-public static class User extends Model {}
-public static final User USER = new User();
-
+public class Tables {
+    @Table("hackaton.User")
+    public static class User extends Model {}
+    public static final User USER = new User();
 }
